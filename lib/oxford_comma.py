070a0bc26bfdb23c177dec3bb41d2d5cd7f50a7c
Robert Gorz <rjgorz@gmail.com>
2023-02-21 08:42:13 -0600

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
+    if(len(items) == 1):
+        return items[0]
+    elif(len(items) == 2):
+        return f"{items[0]} and {items[1]}"
+    elif(len(items) > 2):
+        sentence = ", ".join(items[0:-1])
+        sentence = sentence + f", and {items[-1]}"
+        return sentence
+    else:
+        return "Invalid argument"
     return None
